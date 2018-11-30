409688b84d1e904c45c38dcc4ec43ff708ce49a0
litalsh12 <44766198+litalsh12@users.noreply.github.com>
2018-11-30 14:32:56 +0200

    commit

--- src/File_format/writeKML.java
+++ src/File_format/writeKML.java
@@ -1,34 +1,50 @@
 package File_format;
 import GIS.Project;
+import GIS.layer;
+import GIS.element;
 
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Iterator;
 
 import GIS.CSVReader;
+import GIS.GIS_element;
+import GIS.GIS_layer;
 public class writeKML {
-	BufferedWriter writer;
-	public writeKML(String output)throws IOException{
-		writer = new BufferedWriter(new FileWriter(output));
-		
-		writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-		writer.write("<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Style id=\"blue\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/blue-dot.png</href></Icon></IconStyle></Style><Style id=\"yellow\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/yellow-dot.png</href></Icon></IconStyle></Style><Style id=\"green\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/green-dot.png</href></Icon></IconStyle></Style><Folder><name>Wifi Networks</name>\n");
-		while(iter_layer.hasNext()){
-			GISLayer layer_temp = (GISLayer)iter_layer.next();
-			Iterator<GIS_element> iter_element = layer_temp.iterator();
-			while(iter_element.hasNext())
-			{
-				GISElement elm = (GISElement)iter_element.next();
-				writer.write("<Placemark>\n");
-				writer.write("<name>"+"<![CDATA["+elm.getMd().getSSID()+"]]>"  +"</name>\n");
-				writer.write("<description>"+"<![CDATA[BSSID: <b>"+elm.getMd().getMAC()+"</b><br/>Capabilities: <b>"+elm.getMd().getAuthMode()+"</b><br/>Timestamp: <b>"+elm.getMd().getUTC()+"</b><br/>Channel: <b>"+elm.getMd().getChannel()+"</b><br/>RSSI: <b>"+elm.getMd().getRSSI()+"</b><br/>AltitudeMeters: <b>"+elm.getP().z()+"</b><br/>AccuracyMeters: <b>"+elm.getMd().getAccuracyMeters()+"</b><br/>Type: <b>"+elm.getMd().getType()+"</b><br/>Date: <b>"+elm.getMd().getFirstSeen()+"</b>]]>"+"</description><styleUrl>#blue</styleUrl>\n");
-				writer.write("<Point>\n");
-				writer.write("<coordinates>"+elm.getP().y()+","+elm.getP().x()+"</coordinates></Point>\n");
-				writer.write("</Placemark>\n");
+	static BufferedWriter writer;
+	public writeKML(Project pro, String output)throws IOException{
+		writer = new BufferedWriter(new FileWriter(output));}
 
-			}
+
+
+	public static void write (Project pro) {
+
+		Iterator<GIS_layer> project = pro.iterator();
+		try {
+			writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+			writer.write("<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Style id=\"blue\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/blue-dot.png</href></Icon></IconStyle></Style><Style id=\"yellow\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/yellow-dot.png</href></Icon></IconStyle></Style><Style id=\"green\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/green-dot.png</href></Icon></IconStyle></Style><Folder><name>Wifi Networks</name>\n");
 		}
-		writer.write("</Folder>\n");
-		writer.write("</Document></kml>");
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+		while(project.hasNext()) {
+			GIS_layer lay=project.next();
+			Iterator<GIS_element> element= lay.iterator();
+			while(element.hasNext()){
+				GIS_element elm = element.next();
+				writer.write("<Placemark>\n");
+				writer.write("<name>"+"<![CDATA["+elm.get_SSID()+"]]>"  +"</name>\n");
+				writer.write("<description>"+"<![CDATA[BSSID: <b>"+elm.getMAC()+"</b><br/>Capabilities: <b>"+elm.getAuthMode()+"</b><br/>Timestamp: <b>"+elm.getUTC()+"</b><br/>Channel: <b>"+elm.getMd().getChannel()+"</b><br/>RSSI: <b>"+elm.getMd().getRSSI()+"</b><br/>AltitudeMeters: <b>"+elm.getP().z()+"</b><br/>AccuracyMeters: <b>"+elm.getMd().getAccuracyMeters()+"</b><br/>Type: <b>"+elm.getMd().getType()+"</b><br/>Date: <b>"+elm.getMd().getFirstSeen()+"</b>]]>"+"</description><styleUrl>#blue</styleUrl>\n");
+				writer.write("<Point>\n");
+				writer.write("<coordinates>"+elm.getP().y()+","+elm.x()+"</coordinates></Point>\n");
+				writer.write("</Placemark>\n");
+			}
+
+			writer.write("</Folder>\n");
+			writer.write("</Document></kml>");
+		}
 	}
 }
